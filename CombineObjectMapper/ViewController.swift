a7ca0da7474dc947a601a1fcb99ec7d76ee66f2a
MIhail <mimatyatsko@gmail.com>
2022-08-08 11:56:43 +0300

    Initial Commit

---
+++ CombineObjectMapper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CombineObjectMapper
+//
+//  Created by Bogdan Petkanich on 08.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
